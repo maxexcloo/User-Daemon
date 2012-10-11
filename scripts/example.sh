48c9e728c7793b5843aa3e0e8777152b0e2b3e37
Max Schaefer <max@excloo.com>
2012-10-11 21:09:53 +1100

    Cleaned Up

--- scripts/example.sh
+++ scripts/example.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd ~
-screen -d -m -S example non-backgrounding-daemon
+screen -dmS example daemon
--- scripts/vlc.sh
+++ scripts/vlc.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cd ~/unsorted
+cd ~
 rm -rf ~/.local
-screen -d -m -S vlc cvlc http://www.example.com/ --sout "#http{mux=raw,dst=0.0.0.0:8080/link.pls}"
+screen -dmS vlc cvlc http://www.example.com/ --sout "#http{mux=raw,dst=0.0.0.0:8080/link.pls}"
